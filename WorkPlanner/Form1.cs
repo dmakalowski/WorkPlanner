78dc0339764278c3a0921ffeb22265a4abbeab1d
dmakalowski <grisznak99@gmail.com>
2024-01-30 20:10:38 +0100

    Resolved WP-15 - added database context and linq queries

--- WorkPlanner/Form1.cs
+++ WorkPlanner/Form1.cs
@@ -2,6 +2,7 @@ using System.CodeDom;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq.Expressions;
+using WorkPlanner.Data;
 
 namespace WorkPlanner
 {
@@ -50,27 +51,25 @@ namespace WorkPlanner
                 dateTimePicker1.Focus();
                 return;
             }
-            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Natur\source\repos\WorkPlanner\WorkPlanner\Database1.mdf;Integrated Security=True";
-            SqlConnection connection = new SqlConnection(cs);
-
             try
             {
-                connection.Open();
-                string sql = "INSERT INTO [WorkIntervals](Date, StartTime, EndTime, HoursWorked) VALUES(@param1,@param2,@param3,@param4)";
-                using (SqlCommand cmd = new SqlCommand(sql, connection))
+                using (var connection = new DataClasses1DataContext())
                 {
-                    cmd.Parameters.Add("@param1", SqlDbType.Date).Value = MainCalendar.SelectionRange.Start;
-                    cmd.Parameters.Add("@param2", SqlDbType.Time, 50).Value = dateTimePicker1.Value - dateTimePicker1.Value.Date;
-                    cmd.Parameters.Add("@param3", SqlDbType.Time, 50).Value = dateTimePicker2.Value - dateTimePicker1.Value.Date;
-                    cmd.Parameters.Add("@param4", SqlDbType.Float, 50).Value = (dateTimePicker2.Value.Hour - dateTimePicker1.Value.Hour + (float)(dateTimePicker2.Value.Minute - dateTimePicker1.Value.Minute) / 60);
-                    cmd.CommandType = CommandType.Text;
-                    cmd.ExecuteNonQuery();
+                    var workInterval = new WorkInterval
+                    {
+                        Date = MainCalendar.SelectionRange.Start,
+                        StartTime = dateTimePicker1.Value - dateTimePicker1.Value.Date,
+                        EndTime = dateTimePicker2.Value - dateTimePicker1.Value.Date,
+                        HoursWorked = (dateTimePicker2.Value.Hour - dateTimePicker1.Value.Hour + (float)(dateTimePicker2.Value.Minute - dateTimePicker1.Value.Minute) / 60)
+                    };
+                    connection.WorkIntervals.InsertOnSubmit(workInterval);
+                    connection.SubmitChanges();
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                MessageBox.Show("Error appeared while conecting to database:" + ex.Message);
-                connection.Close();
+
+                throw;
             }
         }
 
